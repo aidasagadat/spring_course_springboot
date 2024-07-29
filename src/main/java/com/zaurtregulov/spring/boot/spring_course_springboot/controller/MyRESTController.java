d8b12616d343e155c7d386a623d1c87f15a16349
Aida Sagadat <aidasagadat355@gmail.com>
2024-07-29 23:28:44 +0500

    Add endpoints to get, add, update and delete entities

--- src/main/java/com/zaurtregulov/spring/boot/spring_course_springboot/controller/MyRESTController.java
+++ src/main/java/com/zaurtregulov/spring/boot/spring_course_springboot/controller/MyRESTController.java
@@ -23,58 +23,50 @@ public class MyRESTController {
     }
 
 
- //
-//
-//    @GetMapping("/employees/{id}")
-//    public Employee getEmployee(@PathVariable int id){
-//        Employee employee = employeeService.getEmployee(id);
-//
-//        if(employee == null){
-//            throw new NoSuchEmployeeException("Employee with id " + id + " not found");
-//        }
-//
-//        return employee;
-//
-//    }
-//
-//
-//    @PostMapping("/employees")
-//    public Employee addNewEmployee(@RequestBody Employee employee){
-//
-//        employeeService.saveEmployee(employee);
-//        return employee;
-//
-//    }
-//
-//
-//
-//    @PutMapping("/employees")
-//    public Employee updateEmployee(@RequestBody Employee employee){
-//
-//        employeeService.saveEmployee(employee);
-//
-//        return employee;
-//
-//    }
-//
-//
-//    @DeleteMapping("/employees/{id}")
-//    public String deleteEmployee(@PathVariable int id){
-//
-//        Employee employee = employeeService.getEmployee(id);
-//
-//        if(employee == null){
-//            throw new NoSuchEmployeeException("Employee with id " + id + " does not exist");
-//        }
-//
-//
-//        employeeService.deleteEmployee(id);
-//
-//        String message = "Employee with id " + id + " was deleted";
-//
-//        return message;
-//
-//    }
+
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+
+        return employee;
+
+    }
+
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee){
+
+        employeeService.saveEmployee(employee);
+        return employee;
+
+    }
+
+
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+
+    }
+
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id){
+
+        Employee employee = employeeService.getEmployee(id);
+
+
+        employeeService.deleteEmployee(id);
+
+        String message = "Employee with id " + id + " was deleted";
+
+        return message;
+
+    }
 
 
 }
